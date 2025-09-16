f82fda0b05f329162d76c7b7249f634022cdaa0d
Aleksey-Shalin <aleksey_ktotamych@mail.ru>
2025-09-16 18:22:23 +0300

    Add files via upload

---
+++ Task2-2_Repeat-a-word.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+#include<windows.h>
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	std::string word;
+	std::cout << "Пожалуйста, введите слово: \n";
+	std::cin >> word;
+	std::cout << " " << '\n';
+	std::cout << "Вы ввели слово: \n" << word;
+	return 0;
+}
