c279c42820944425f72a949319f6b6c4cb6a21a2
Aleksey-Shalin <aleksey_ktotamych@mail.ru>
2025-09-16 18:18:53 +0300

    Add files via upload

---
+++ Task2-1_Repeat-a-number.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+int main()
+{
+	setlocale(LC_CTYPE, "");
+	double number;
+	std::cout << "Пожалуйста, введите любое число: \n";
+	std::cin >> number;
+	std::cout << " " << '\n'; // Здесь пробел между строками. Вероятно, есть другой способ реализации, но меня посетил только этот
+	std::cout << "Вы ввели число: \n" << number;
+	return 0;
+}
---
+++ Task2-2_Repeat-a-sentence.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+#include<windows.h>
+#include<string>
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	std::string sentence;
+	std::cout << "Пожалуйста, введите предложение: \n";
+	std::getline(std::cin >> std::ws, sentence);
+	std::cout << " " << '\n';
+	std::cout << "Вы ввели предложение: \n" << sentence;
+	return EXIT_SUCCESS;
+}
